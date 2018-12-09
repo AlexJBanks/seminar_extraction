e32f8505b300d5776fdfdc613c9b3bf53fd9be3e
Alex Banks <alex.banx98@gmail.com>
2018-12-09 00:24:42 +0000

    refactoring

--- EntityTagging.py
+++ EntityTagging.py
@@ -1,49 +1,45 @@
 import re
 
-file0 = open("data/email/pretagged/0.txt").read()
+fullEmail = open('data/email/pretagged/0.txt').read()
+fullEmail.split('')
 
-timeExp = r"(?i)(?P<hrs>\d{1,2})(?::(?P<min0>\d{2}) ?(?P<ap0>[ap]).?m|:(?P<min1>\d{2})| ?(?P<ap1>[ap]).?m)"
 paraExp = r"(^|\n)[A-Z].+(\n.+)+?[.:!?](\n|$)"
+paraIter = re.compile(paraExp).finditer(fullEmail)
 
-timeIter = re.compile(timeExp).finditer(file0)
-paraIter = re.compile(paraExp).finditer(file0)
+def tag_times() :
+    time_exp = r"(?i)(?P<hrs>\d{1,2})(?::(?P<min0>\d{2}) ?(?P<ap0>[ap]).?m|:(?P<min1>\d{2})| ?(?P<ap1>[ap]).?m)"
+    time_iter = re.compile(time_exp).finditer(fullEmail)
 
-times = {}
-mintime = 2400
-maxtime = 0
-for timeObj in enumerate(timeIter):
+    times = {}
+    mintime = 2400
+    maxtime = 0
+    for timeObj in enumerate(time_iter):
 
-    valid = True
-    time = int(timeObj[1].group('hrs'))*100
+        valid = True
+        time = int(timeObj[1].group('hrs'))*100
 
-    if (timeObj[1].group('ap0') is not None and timeObj[1].group('ap0').lower() == 'p')\
-            or (timeObj[1].group('ap1') is not None and timeObj[1].group('ap1').lower() == 'p'):
-        time = time + 1200
+        if (timeObj[1].group('ap0') is not None and timeObj[1].group('ap0').lower() == 'p')\
+                or (timeObj[1].group('ap1') is not None and timeObj[1].group('ap1').lower() == 'p'):
+            time = time + 1200
 
-    if time > 2400: valid = False
+        if time > 2400: valid = False
 
-    mins = 0
-    if timeObj[1].group('min0') is not None:
-        mins = int(timeObj[1].group('min0'))
-    if timeObj[1].group('min1') is not None:
-        mins = int(timeObj[1].group('min1'))
+        mins = 0
+        if timeObj[1].group('min0') is not None:
+            mins = int(timeObj[1].group('min0'))
+        if timeObj[1].group('min1') is not None:
+            mins = int(timeObj[1].group('min1'))
 
-    if mins > 59 or mins < 0: valid = False
-    time = time + mins
+        if mins > 59 or mins < 0: valid = False
+        time = time + mins
 
-    if valid:
-        times[time] = timeObj
-        mintime = min(time, mintime)
-        maxtime = max(time, maxtime)
+        if valid:
+            times[time] = timeObj
+            mintime = min(time, mintime)
+            maxtime = max(time, maxtime)
 
-print(times[mintime])
-print(times[maxtime])
-
-
-
-
-
-
+    print(times[mintime])
+    print(times[maxtime])
 
 # TODO stime
 # TODO etime
