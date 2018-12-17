660d8d257a08f2e89ba71f0e9cf9e90177d2c534
Alex Banks <alex.banx98@gmail.com>
2018-12-17 04:33:46 +0000

    ontology starter

--- Ontology.py
+++ Ontology.py
@@ -1,15 +1,60 @@
+import re
+from collections import Counter
 from os import listdir
 from os.path import isfile, join
+from nltk.corpus import brown
 
 myPath = 'data/email/training/'
 onlyFiles = [f for f in listdir(myPath+'untagged/') if isfile(join(myPath+'untagged/', f))]
 
 
-def rank_words(email):
-    full_email = open(myPath + 'untagged/' + str(email)).read()
-    pass
+def words(text): return re.findall(r"(?im)(?<=[\s\-(])(\w*[a-z]\w*)(?=[.,'):\-\/\s])", text.lower())
 
 
-for email in onlyFiles:
-    print(email)
-    rank_words(email)
+def get_prob(frequencies):
+    probability = {}
+    total = sum(frequencies.values())
+    for word in frequencies.most_common():
+        probability[word[0]] = word[1] / total
+    return probability
+
+
+def rank_all():
+    all_head = []
+    for email in onlyFiles:
+        full_email = open(myPath + 'untagged/' + str(email)).read()
+        if 'Topic:' in full_email:
+            for word in words(open(myPath + 'untagged/' + str(email)).read().split('Topic:')[1].split(':')[0]):
+                all_head.append(word)
+    head_freq = Counter(all_head)
+    head_prob = get_prob(head_freq)
+
+    brown_words = []
+    for word in brown.words():
+        brown_words.append(word.lower())
+    brown_freq = Counter(brown_words)
+    brown_prob = get_prob(brown_freq)
+
+    delta = {}
+    for word in head_prob:
+        print(word)
+        print(head_prob[word])
+        if word in brown_prob:
+            print(brown_prob[word])
+            delta[word] = head_prob[word] - brown_prob[word]
+        else:
+            print()
+            delta[word] = head_prob[word]
+
+    sorted_delta = sorted(delta, key=delta.get, reverse=True)
+    print(sorted_delta)
+
+    file_content = ""
+    for word in sorted_delta:
+        file_content = file_content + word + '\n'
+
+    with open('data/normalised_word_popularity.txt', 'w') as head_file:
+        head_file.write(file_content[:-1])
+
+
+rank_all()
---
+++ ontologyEvaluation.md
@@ -0,0 +1,43 @@
+# Ontology Evaluation
+###### Alex Banks
+
+## General Approach
+Ontology went through a series of 
+
+### 1. Gathering Data
+The first step was to gather some data.
+I wanted to know which words were most popular in the emails,
+but needed to account for some words being popular anyway.
+
+This was achieved by finding the popularity of words in the training emails,
+the popularity of words in a large corpus (I used `brown`),
+and subtracting brown popularity from the email popularity.
+This data is then ordered with the most unique words first,
+and saved to `normalised_word_popularity.txt`.
+An excerpt:
+```
+cmu
+cs
+edu
+...
+to
+of
+the
+```
+
+This was further improved by reducing the scope, first to look at just `Topic:` segment
+```regexp
+(?m)^(.+?):\s*(.*(?:\n\s+.*)*)
+```
+
+Also, buy tweaking the definition of a 'word' so that parts of email etc weren't included
+```regexp
+(?i)(?<=[\s\-(])(\w*[a-z]\w*)(?=[.,')\-\/\s])
+``` 
+
+### 2. Defining Departments
+
+## Accuracy
+
+
+## Further Improvements
